c29ecc35eef509e3dd4a3a80493a6b146af70723
manuel <manuel.payan@pccomponentes.com>
2020-10-09 16:49:41 +0200

    configuracion base strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cb280442bf1a2e875cb0cfc9e405491c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1c3d23a1-563b-4fcb-9819-dcba1879fae5'
+};
